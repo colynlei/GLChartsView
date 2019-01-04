5a7f15befeb3394817e8fc7256f870881735bb1f
『国』 <leiguolin@huobi.com>
2019-01-04 10:34:43 +0800

    创建新的工程

---
+++ GLChartsView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GLChartsView
+//
+//  Created by 雷国林 on 2019/1/4.
+//  Copyright © 2019 雷国林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GLChartsView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GLChartsView
+//
+//  Created by 雷国林 on 2019/1/4.
+//  Copyright © 2019 雷国林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
